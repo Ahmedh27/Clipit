1c947cc3e8fe9abe40a29b794af7dfb44c8d84b8
Kenneth Romero <romerokenny882@gmail.com>
2024-12-10 21:00:45 -0500

    COmmit after file errors

---
+++ Clipit-frontend/.nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/ahmed/Clipit/Clipit-frontend/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ Clipit-frontend/.nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/ahmed/Clipit/Clipit-frontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
